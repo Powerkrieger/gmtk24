715172d3f39235de932e4aa76679258a1a7c1d42
yaz <yaz@fedora.fritz.box>
2024-08-17 14:09:15 +0200

    removed camera, fixed ui, moved base layer

--- scenes/Buildings/housingbuildingpanel.gd
+++ scenes/Buildings/housingbuildingpanel.gd
@@ -38,7 +38,7 @@ func _on_gui_input(event: InputEvent) -> void:
 		if event is InputEventMouseMotion and event.button_mask == MOUSE_BUTTON_LEFT:
 			get_child(1).global_position = event.global_position
 			var mapPath = get_tree().get_root().get_node("Main/Tilemap")
-			var tile = mapPath.local_to_map(get_global_mouse_position())
+			var tile = mapPath.local_to_map(mapPath.to_local(get_global_mouse_position()))
 			currTileLayer0 = mapPath.get_cell_atlas_coords(0, tile, false)  # 0 -> layer of tilemap
 			
 			if (currTileLayer0 == Vector2i(1,1)):
